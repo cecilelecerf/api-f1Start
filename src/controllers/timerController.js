74b95bf029b69bd435aed6683336f3da6edd7c98
cecilelecerf <c.lecerf.cecile@gmail.com>
2023-12-12 16:12:17 +0100

    create a createTimer in timerController

---
+++ src/controllers/timerController.js
@@ -0,0 +1,15 @@
+const Timer = require("../models/timerModel")
+const User = require("../models/userModel")
+
+exports.createTimer = async(req, res)=>{
+    try{
+        const newTimer = new Timer(req.params.id_user, req.body);
+        if(new User.findById(newTimer.user_id))
+            nullifiable()
+        const timer = await newTimer.save();
+        res.status(201).json({timer});
+    } catch (error){
+        console.log(error)
+        res.status(500).json({message: "Error server."})
+    }
+}
